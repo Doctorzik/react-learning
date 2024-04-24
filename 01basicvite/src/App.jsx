469e6e19f8b291a371ec649b732adfe804c963d7
Doctorzik <ezekieljeremiah92@gmail>
2024-04-25 00:04:39 +0100

    hh

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./header";
+
+function App() {
+  return (
+    <>
+      <h1>This is Jeremiah Ezekiel</h1>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+  return(
+  <h2> I arguments now a man </h2>
+)
+  
+}
+
+export default Header;
